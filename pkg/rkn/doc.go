9de74e93e95617eb227c5b871087016bf59aade7
kc merrill <kcmerrill@gmail.com>
2018-08-16 21:11:02 -0600

    Initial updates ... again

--- pkg/rkn/doc.go
+++ pkg/rkn/doc.go
@@ -1,5 +1,45 @@
 package rkn
 
+import (
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/golang/snappy"
+)
+
+// Doc returns a newly created document
+func Doc(key, contents string) *Document {
+	return &Document{
+		Key:       key,
+		Contents:  contents,
+		TimeStamp: time.Now(),
+	}
+}
+
 // Document ...
 type Document struct {
+	TimeStamp time.Time `json:"timestamp"`
+	Contents  string    `json:"contents"`
+	Key       string    `json:"key"`
+	sync.Mutex
+}
+
+// Touch ...
+func (d *Document) Touch() time.Time {
+	d.Lock()
+	defer d.Unlock()
+
+	d.TimeStamp = time.Now()
+	return d.TimeStamp
+}
+
+// toString()
+func (d *Document) toString() (string, string, string) {
+	d.Lock()
+	defer d.Unlock()
+
+	compressed := snappy.Encode(nil, []byte(d.Contents))
+	return strings.Replace(d.Key, "|", "-", -1), strconv.FormatInt(d.TimeStamp.Unix(), 10), string(compressed)
 }
--- pkg/rkn/rkn.go
+++ pkg/rkn/rkn.go
@@ -1,21 +1,58 @@
 package rkn
 
 import (
-	"os"
+	"fmt"
+	"sync"
 )
 
-// Rkn ...
-type Rkn struct {
+// DB ...
+type DB struct {
 	// private
-	toDiskHandle os.File
+	toDisk   chan string // does this need to write to disk?
+	flush    chan bool   // when to flush to disk
+	inMemory bool        // does this persist to memory?
+	cache    map[string]*Document
+
+	// global db lock
+	sync.Mutex
 }
 
 // Open ...
-func (r *Rkn) Open(store string) {
+func Open(store string) (*DB, error) {
+	d := &DB{}
 
+	// are we in memory?
+	d.inMemory = d.isInMemory(store)
+
+	// init our maps
+	d.segments = make(map[string]int64)
+
+	// init our channels
+	d.toDisk = make(chan string)
+
+	go d.Broker()
+	return d, nil
 }
 
 // Save ...
-func (r *Rkn) Save(d *Document) bool {
-	return true
+func (d *DB) Save(doc *Document) {
+	key, ts, contents := doc.toString()
+	d.toDisk <- fmt.Sprintf("{%s|%s|%d}%s", key, ts, len(contents), contents)
+}
+
+// Fetch ...
+func (d *DB) Fetch(key string) *Document {
+	return &Document{}
+}
+
+// isInMemory() determines if the store is in memory
+func (d *DB) isInMemory(store string) bool {
+	return store == ":in-memory:"
+}
+
+// Broker ...
+func (d *DB) Broker() {
+	for {
+		select {}
+	}
 }
